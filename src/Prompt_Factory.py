ae733852670bd7b77e974a9c0d95945ee890fc8a
Ahmet Çağatay Savaşlı <cagatay.savasli@ozu.edu.tr>
2024-11-24 00:08:23 +0300

    Add Promp Factory with first prompt generation strategy

---
+++ src/Prompt_Factory.py
@@ -0,0 +1,19 @@
+def prompt_factory(strategy, row):
+    if strategy == 1:
+        return strategy_1(row)
+
+
+def strategy_1(row):
+    prompt = (
+        "You are a scientific assistant tasked with summarizing biomedical research papers. "
+        "The goal is to create a simple and clear lay summary for a general audience. "
+        "Here is the title, abstract and selected sentence from sections of the paper:\n\n"
+        "Title:\n"
+        f"{row['title']}\n"
+        "Abstract:\n"
+        f"{' '.join(map(str, row['abstract']))}\n"  # Ensure all elements in 'abstract' are strings
+        "Selected sentence:\n"
+        f"{'\n'.join(map(str, row['rag_sentences']))}\n"  # Ensure all elements in 'rag_sentences' are strings
+        "\n\nUsing the given information, write a lay summary in simple terms:"
+    )
+    return prompt
