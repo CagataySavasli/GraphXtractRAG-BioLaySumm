dece8e2be56982df068a6a798f1a09d35a459fa3
Ahmet Çağatay Savaşlı <cagatay.savasli@ozu.edu.tr>
2024-12-06 20:33:36 +0300

    Add without RAG strategy

--- src/Prompt_Factory.py
+++ src/Prompt_Factory.py
@@ -1,9 +1,23 @@
 def prompt_factory(strategy, row):
     if strategy == 1:
         return strategy_1(row)
-
+    elif strategy == 2:
+        return strategy_2(row)
 
 def strategy_1(row):
+    prompt = (
+        "You are a scientific assistant tasked with summarizing biomedical research papers. "
+        "The goal is to create a simple and clear lay summary for a general audience. "
+        "Here is the title and abstract of the paper:\n\n"
+        "Title:\n"
+        f"{row['title']}\n"
+        "Abstract:\n"
+        f"{' '.join(map(str, row['abstract']))}\n"  # Ensure all elements in 'abstract' are strings
+        "\n\nUsing the given information, write a lay summary in simple terms:"
+    )
+    return prompt
+
+def strategy_2(row):
     prompt = (
         "You are a scientific assistant tasked with summarizing biomedical research papers. "
         "The goal is to create a simple and clear lay summary for a general audience. "
