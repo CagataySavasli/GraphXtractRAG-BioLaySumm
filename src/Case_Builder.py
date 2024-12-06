88f163b644047405a204c5629d7a0f3285e98f06
Ahmet Çağatay Savaşlı <cagatay.savasli@ozu.edu.tr>
2024-12-06 20:33:06 +0300

    Add massage strategy and BioMixtral

--- src/Case_Builder.py
+++ src/Case_Builder.py
@@ -3,13 +3,18 @@ import torch
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 #device = torch.device('cpu')
 
+massage_strategy = 'zero_shot'
+
 dataset_name = "elife"
-prompt_strategy_used = 1
+prompt_strategy_used = 2
 bert_version = 'BioBERT'
 genai_version = 't5'
 
 if bert_version == 'BioBERT':
     bert_model_name = 'dmis-lab/biobert-base-cased-v1.2'
 
+
 if genai_version == 't5':
     genai_model_name = 'google/flan-t5-small'
+elif genai_version == 'BioMistral':
+    genai_model_name = 'BioMistral/BioMistral-7B'
