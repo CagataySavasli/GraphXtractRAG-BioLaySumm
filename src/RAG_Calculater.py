74228e8552b0b708f7e875c031d08b732f862509
Ahmet Çağatay Savaşlı <cagatay.savasli@ozu.edu.tr>
2024-12-06 20:32:02 +0300

    Add same RAG approach for selecting reference articles

--- src/RAG_Calculater.py
+++ src/RAG_Calculater.py
@@ -1,4 +1,5 @@
-
+import torch
+import numpy as np
 def iter_result_list(result_list, top_sentence, n):
     n_goal = len(result_list) + n
 
@@ -9,21 +10,34 @@ def iter_result_list(result_list, top_sentence, n):
                 break
     return result_list
 def get_top_n_sentences(sentences, scores, n):
-    # Combine sentences and avg values into a single list
     combined = list(zip(scores, sentences))
-
-    # Sort by avg values in descending order
     sorted_combined = sorted(combined, key=lambda x: x[0], reverse=True)
 
-    # Get top n sentences with highest avg values
     top_n = sorted_combined[:n]
-
-    # Extract sentences and avg values separately
     top_n_sentences = [item[1] for item in top_n]
 
     return top_n_sentences
 
 
+def get_top_n_articles(train_embedings, target_embeding, n):
+    # Numpy array'lerini float32 formatına çevir
+    train_embedings = np.array([np.array(embed, dtype=np.float32) for embed in train_embedings])
+    target_embeding = np.array(target_embeding, dtype=np.float32)
+
+    # PyTorch tensörüne dönüştür
+    train_embedings = torch.tensor(train_embedings)
+    target_embeding = torch.tensor(target_embeding)
+
+    # Kosinüs benzerliğini hesapla
+    similarities = torch.nn.functional.cosine_similarity(
+        train_embedings.unsqueeze(1),  # Genişletme
+        target_embeding.unsqueeze(0),  # Genişletme
+        dim=2
+    ).squeeze()
+
+    _, top_n_indices = torch.topk(similarities, n, dim=0)
+
+    return top_n_indices.tolist()
 def RAG(row):
     n = 10
     result = {
