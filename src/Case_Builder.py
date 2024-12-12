a9ac81d53fe119a6bdb6e924a04807aed7594327
Ahmet Çağatay Savaşlı <cagatay.savasli@ozu.edu.tr>
2024-12-12 16:37:10 +0300

    Fix case builder

--- src/Case_Builder.py
+++ src/Case_Builder.py
@@ -3,12 +3,13 @@ import torch
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 #device = torch.device('cpu')
 
+genai_version = 'BioMistral'
 massage_strategy = 'zero_shot'
-
-dataset_name = "elife"
-prompt_strategy_used = 2
 bert_version = 'BioBERT'
-genai_version = 't5'
+dataset_name = "elife"
+prompt_strategy_used = 1
+
+
 
 if bert_version == 'BioBERT':
     bert_model_name = 'dmis-lab/biobert-base-cased-v1.2'
@@ -18,3 +19,5 @@ if genai_version == 't5':
     genai_model_name = 'google/flan-t5-small'
 elif genai_version == 'BioMistral':
     genai_model_name = 'BioMistral/BioMistral-7B'
+elif genai_version == 'BioGBT':
+    genai_model_name = 'microsoft/BioGPT-Large-PubMedQA'#'microsoft/biogpt'
