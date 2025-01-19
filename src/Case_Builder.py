0e0d44a5846568eeb147d612674c17e26021920a
Ahmet Çağatay Savaşlı <cagatay.savasli@ozu.edu.tr>
2025-01-19 23:47:43 +0300

    Implement singleton design pattern

--- src/Case_Builder.py
+++ src/Case_Builder.py
@@ -1,11 +1,26 @@
 import torch
 
 
-class CaseBuilder:
-    #device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+class SingletonMeta(type):
+    """
+    Singleton metaclass: Her sınıf için yalnızca bir örnek oluşturulmasını sağlar.
+    """
+    _instances = {}
+
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            # Sınıf için ilk kez bir örnek oluşturuluyor
+            instance = super().__call__(*args, **kwargs)
+            cls._instances[cls] = instance
+        return cls._instances[cls]
+
+
+class CaseBuilder(metaclass=SingletonMeta):
+    # device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     device = torch.device('cpu')
 
     def __init__(self, genai_version, massage_strategy, bert_version, dataset_name, prompt_strategy_used):
+        # Bu sınıf zaten oluşturulduysa tekrar initialize edilmeyecek
         self.genai_version = genai_version
         self.massage_strategy = massage_strategy
         self.bert_version = bert_version
