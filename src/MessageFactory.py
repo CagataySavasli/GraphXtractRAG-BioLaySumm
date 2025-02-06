f289b9eb5d401c009ce21c4950435e757bf68bad
Ahmet Çağatay Savaşlı <cagatay.savasli@ozu.edu.tr>
2025-02-06 18:10:31 +0300

    Add typing

--- src/MessageFactory.py
+++ src/MessageFactory.py
@@ -1,8 +1,9 @@
 from src.prompt_factories.PromptFactory import PromptFactory
 from src.rag_factories.RAG_Factory import RAG_Factory
+import pandas as pd
 
 
-class MassegeFactory:
+class MessageFactory:
     def __init__(self, case_builder, n, n_2=None, n_3=None):
         self.case_builder = case_builder
         self.n = n
@@ -12,33 +13,32 @@ class MassegeFactory:
         self.prompt_generator = PromptFactory(self.case_builder)
         self.rag_calculator = RAG_Factory(self.case_builder, self.n, self.n_2, self.n_3)
 
-    def prepare_case(self, row):
+    def prepare_case(self, row: pd.Series):
         self.rag_calculator.set_row(row)
         row['rag_sentences'] = self.rag_calculator.get_n_sentences()
         self.prompt_generator.set_row(row)
 
-    def get_instruction(self):
+    def get_instruction(self) -> str:
         instruction = self.prompt_generator.get_instruction()
         return instruction
 
-    def get_prompt(self, row):
+    def get_prompt(self, row: pd.Series) -> str:
         self.prepare_case(row)
         prompt = self.prompt_generator.get_prompt()
         return prompt
 
-    def get_info(self, row):
+    def get_info(self, row: pd.Series) -> str:
         self.prepare_case(row)
         info = self.prompt_generator.get_info()
         return info
 
-
-    def create_massege(self, row):
+    def create_message(self, row: pd.Series) -> dict:
         prompt = self.get_prompt(row)
         summary = str(row['summary'])
 
         return {"text_input": prompt, "output": summary}
 
-    def create_massege_prompting(self, row):
+    def create_message_prompting(self, row: pd.Series) -> dict:
         prompt = self.get_info(row)
         summary = str(row['summary'])
 
