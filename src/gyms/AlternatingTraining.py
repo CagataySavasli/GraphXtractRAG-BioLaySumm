0d20ce8a2246f5664b235812215d382febbe48c4
Ahmet Çağatay Savaşlı <cagatay.savasli@ozu.edu.tr>
2025-03-02 20:32:32 +0300

    Add ALternating Traing for GES-RAG and Gemini

---
+++ src/gyms/AlternatingTraining.py
@@ -0,0 +1,41 @@
+
+class AlternatingTraining:
+    def __init__(self, selector_gym, gemini_gym, num_episodes=10):
+        self.selector_gym = selector_gym
+        self.gemini_gym = gemini_gym
+    def alternating_training(self,num_episodes=10):
+        """
+        Alternating training between SelectorGYM and GeminiGYM for num_episodes.
+
+        Args:
+            num_episodes (int): Number of epochs for alternating training.
+        """
+        iter_epoch = 1
+        for epoch in range(num_episodes):
+            print(f"Epoch {epoch + 1}/{num_episodes} | Iteration {iter_epoch}")
+            if epoch % 2 == 0:
+                print("Fine-tuning GeminiGYM...")
+                self.gemini_gym.fine_tune(
+                    display_name=f"gemini_finetune_mix_epoch_{epoch}",
+                    epoch_count=iter_epoch + 1,
+                    batch_size=8,  # Adjust batch size as needed
+                    learning_rate=5e-5  # Adjust learning rate as needed
+                )
+                self.gemini_gym.update_model()
+
+            else:
+                print("Training SelectorGYM...")
+                self.selector_gym.train(iter_epoch)  # Train selector for 1 epoch
+                self.selector_gym.save_selector()
+                iter_epoch += 1
+
+            print("#" * 50)
+            print(self.gemini_gym.base_model)
+            print(self.selector_gym.genai_model_name)
+            print("-" * 50)
+            self.selector_gym.genai_model_name = self.gemini_gym.base_model
+            self.selector_gym.reset_generaoi_model()
+
+            print(self.gemini_gym.base_model)
+            print(self.selector_gym.genai_model_name)
+            print(f"Epoch {epoch + 1} completed.\n")
