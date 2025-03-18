dbb5aac3250de51b3633473586b7ff1122abd826
Ahmet Çağatay Savaşlı <cagatay.savasli@ozu.edu.tr>
2025-03-18 15:33:25 +0300

    Add print for follow process

--- Trainer.py
+++ Trainer.py
@@ -7,33 +7,42 @@ import pandas as pd
 import sys
 #%%
 rag_strategy = sys.argv[1] #"similarityRAG"
+print("RAG Strategy: ", rag_strategy)
 #%%
 case_builder = CaseBuilder(rag_strategy=rag_strategy)
 result_calculator = ResultCalculator()
 #%%
+print("Dataset Name: ", case_builder.dataset_name)
 df_train = pd.read_json(f'dataset/processed/{case_builder.dataset_name}/train.json').reset_index(drop=True)
 df_test = pd.read_json(f'dataset/processed/{case_builder.dataset_name}/test.json').reset_index(drop=True)
+print("Train Shape: ", df_train.shape)
+print("Test Shape: ", df_test.shape)
 #%%
 gemini_gym = GeminiGYM()
 gemini_gym.set_train_data(df_train.copy())
+print("GeminiGYM Created")
 #%%
 gemini_gym.fine_tune(display_name=rag_strategy,
                      epoch_count=20,
                      batch_size=8,
                      learning_rate=5e-4)
+print("Fine Tuning Completed")
 #%%
 gemini_gym.set_test_data(df_test.copy())
 test_results = gemini_gym.evaluate()
+print("Evaluation Completed")
 #%%
 predicted = test_results['pred']
 actual = test_results['true']
 #%%
 score_dict = result_calculator.evaluate(predicted, actual)
+print("Score Calculated")
 #%%
 generated_text_df = pd.DataFrame({'generated': predicted, 'actual': actual})
 generated_text_df.to_csv(f'outputs/generated_texts/{case_builder.rag_strategy}_{case_builder.dataset_name}_genrated_text.csv', index=False)
 score_df = pd.DataFrame([score_dict])
 score_df.to_csv(f'outputs/scores/{case_builder.rag_strategy}_{case_builder.dataset_name}_score.csv', index=False)
+print("Results Saved")
 #%%
 result_calculator.display_rank(score_dict)
 #%%
