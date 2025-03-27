add07f7aadc85b7132767d3c92bfd9ce32d3747d
Ahmet Çağatay Savaşlı <cagatay.savasli@ozu.edu.tr>
2025-03-28 00:23:13 +0300

    Edit for parallel run in cluster

--- GetEmbedding.py
+++ GetEmbedding.py
@@ -13,10 +13,16 @@ bert_model = 'BioBERT'
 dataset = sys.argv[1]
 dataset_info = sys.argv[2]
 
+lower_bound = int(sys.argv[4])
+upper_bound = int(sys.argv[3])
+
+
 case_builder = CaseBuilder(genai_type, bert_model, message_type, rag_type, rag_strategy, dataset)
 
 # Load the dataset
 df = pd.read_json(f'dataset/raw/{dataset}/{dataset_info}.json')
+
+df = df[lower_bound:upper_bound].copy()
 
 dataset_generator = DatasetGenerator()
 dataset_generator.set_data(df)
@@ -24,4 +30,4 @@ dataset_generator.preprocess()
 data = dataset_generator.get_data()
 
 print(df.shape, data.shape)
-data.to_json(f'dataset/processed/{dataset}/{dataset_info}.json')
+data.to_json(f'dataset/processed/{dataset}/sep/{dataset_info}_{lower_bound}_{upper_bound}.json')
