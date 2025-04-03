de31adbc5c9f217d3d8ed66ef260e19525e89606
Ahmet Çağatay Savaşlı <cagatay.savasli@ozu.edu.tr>
2025-04-03 21:13:43 +0300

    Change display name (rag_strategy + rag_n + dataset_name)

--- Trainer.py
+++ Trainer.py
@@ -7,9 +7,12 @@ import pandas as pd
 import sys
 #%%
 rag_strategy = sys.argv[1] #"similarityRAG"
+rag_n = int(sys.argv[2]) # 30
 print("RAG Strategy: ", rag_strategy)
+print("RAG N: ", rag_n)
 #%%
-case_builder = CaseBuilder(rag_strategy=rag_strategy)
+case_builder = CaseBuilder(rag_strategy=rag_strategy,
+                           rag_n=rag_n)
 result_calculator = ResultCalculator()
 #%%
 print("Dataset Name: ", case_builder.dataset_name)
@@ -22,7 +25,7 @@ gemini_gym = GeminiGYM()
 gemini_gym.set_train_data(df_train.copy())
 print("GeminiGYM Created")
 #%%
-display_name = f"{case_builder.rag_strategy}_{case_builder.dataset_name}"
+display_name = f"{case_builder.rag_strategy}_{case_builder.rag_n}_{case_builder.dataset_name}"
 gemini_gym.fine_tune(display_name=display_name,
                      epoch_count=20)
 print("Fine Tuning Completed")
@@ -38,9 +41,9 @@ score_dict = result_calculator.evaluate(predicted, actual)
 print("Score Calculated")
 #%%
 generated_text_df = pd.DataFrame({'generated': predicted, 'actual': actual})
-generated_text_df.to_csv(f'outputs/generated_texts/{case_builder.rag_strategy}_{case_builder.dataset_name}_genrated_text.csv', index=False)
+generated_text_df.to_csv(f'outputs/generated_texts/{case_builder.rag_strategy}_{case_builder.rag_n}_{case_builder.dataset_name}_genrated_text.csv', index=False)
 score_df = pd.DataFrame([score_dict])
-score_df.to_csv(f'outputs/scores/{case_builder.rag_strategy}_{case_builder.dataset_name}_score.csv', index=False)
+score_df.to_csv(f'outputs/scores/{case_builder.rag_strategy}_{case_builder.rag_n}_{case_builder.dataset_name}_score.csv', index=False)
 print("Results Saved")
 #%%
 result_calculator.display_rank(score_dict)
