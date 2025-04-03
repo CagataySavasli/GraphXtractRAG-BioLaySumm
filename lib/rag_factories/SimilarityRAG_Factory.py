011ba200aee361f826c250e974e80eae01b00009
Ahmet Çağatay Savaşlı <cagatay.savasli@ozu.edu.tr>
2025-04-03 22:46:22 +0300

    Remove abstract and keywords calculations

--- lib/rag_factories/SimilarityRAG_Factory.py
+++ lib/rag_factories/SimilarityRAG_Factory.py
@@ -5,23 +5,16 @@ from sklearn.metrics.pairwise import cosine_similarity
 
 class SimilarityRAG(AbstractRAG_Factory):
 
-    def __init__(self, case, title_n, abstract_n = 0, keywords_n = 0):
+    def __init__(self, case, n):
         print("SimilarityRAG Factory")
 
         self.case = case
-        self.title_n = title_n
-        self.abstract_n = abstract_n
-        self.keywords_n = keywords_n
-
-        self.n = self.title_n + self.abstract_n + self.keywords_n
+        self.n = n
 
         self.row = None
 
         self.nodes = None
         self.sentences = None
-
-        self.ref_similarity = None
-        self.selected_indexes = []
 
         self.top_bottom_checker = None
 
@@ -32,25 +25,13 @@ class SimilarityRAG(AbstractRAG_Factory):
         self.sentences = [x for y in row['sections'] for x in y]
 
     def reset(self):
-        self.selected_indexes = []
         self.nodes = None
         self.sentences = None
-        self.ref_similarity = None
 
     def calculate_similarities(self):
-
-        referances = [np.array(ref) for ref in
-                      self.row[['title_embedding', 'abstract_embedding', 'keywords_embedding']]]
-        similarity = cosine_similarity(
-            referances,
-            self.nodes,
-        ).tolist()
-
-        self.ref_similarity = {
-            'title': similarity[0],
-            'abstract': similarity[1],
-            'keywords': similarity[2]
-        }
+        references = np.array(self.row['title_embedding'], dtype=float)
+        similarity = cosine_similarity([references], self.nodes).tolist()
+        return similarity[0]
 
     def get_top_n_index(self, similarity):
         index = [x for x in range(len(similarity))]
@@ -64,25 +45,6 @@ class SimilarityRAG(AbstractRAG_Factory):
 
         return top_n_sentences
 
-    def iter_selecte_list(self, index, N):
-        n_goal = len(self.selected_indexes) + N
-
-        i = 0
-        while len(self.selected_indexes) < n_goal:
-            if not index[i] in self.selected_indexes:
-                self.selected_indexes.append(index[i])
-            i += 1
-
-    def selecte_n_index(self):
-
-        title_top_n_index = self.get_top_n_index(self.ref_similarity['title'])
-        abstract_top_n_index = self.get_top_n_index(self.ref_similarity['abstract'])
-        keywords_top_n_index = self.get_top_n_index(self.ref_similarity['keywords'])
-
-        self.iter_selecte_list(title_top_n_index, self.title_n, )
-        self.iter_selecte_list(abstract_top_n_index, self.abstract_n, )
-        self.iter_selecte_list(keywords_top_n_index, self.keywords_n, )
-
     def selector(self):
         if self.case == 'top':
             self.top_bottom_checker = True
@@ -92,10 +54,10 @@ class SimilarityRAG(AbstractRAG_Factory):
             return None
     def get_n_sentences(self):
         self.selector()
-        self.calculate_similarities()
-        self.selecte_n_index()
+        similarity = self.calculate_similarities()
+        selected_indexes = self.get_top_n_index(similarity)
 
-        return [self.sentences[i] for i in self.selected_indexes]
+        return [self.sentences[i] for i in selected_indexes]
 
 
 
